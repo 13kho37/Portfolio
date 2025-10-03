a1dbab4f27a35213f473bc8dae4274db1eb21dda
4lizee <alizeebochaton17@gmail.com>
2025-10-03 18:52:03 +0200

    functional carousel js + networking project page + padding responsive

--- script.js
+++ script.js
@@ -14,11 +14,18 @@ const projets = [
         link: "reseau_project.html"
     },
     {
-        title: "Administration et supervision réseau",
-        description:"Création d'un réseau de machines virtuelles avec Proxmox et OpnSense",
-        features: ["Virtualisation de machine", "Configuration de proxy et reverse proxy", "Administration windows server (DHCP, DNS, AD)", "Configuration de wazuh"],
-        image: "assets/carousel_reseau.webp",
-        link: "reseau_project.html"
+        title: "IoT / Systèmes embarqués",
+        description:"Développement d’un système de lecture de badges RFID avec un ESP32, relié à une API pour gérer les autorisations",
+        features: ["ESP-32", "Framework arduino", "Communication avec une API", "Module RFID RC522"],
+        image: "assets/iot_carousel.jpg",
+        link: "iot.html"
+    },
+    {
+        title: "Premiers pas en reverse engineering",
+        description:"Analyse d'un objet connecté pour comprendre son fonctionnement et ses failles de sécurité",
+        features: ["Démontage matériel et inspection du PCB", "Identification des composants électroniques", "Récupération et analyse des interfaces (UART, SPI, etc.)"],
+        image: "assets/reverse_carousel.jpg",
+        link: "reverse_project.html"
     }
 ];
 
@@ -35,9 +42,9 @@ function createProjectCard(screenSize) {
             projectContainer.innerHTML += `
             <div class="carousel-item ${projectContainer.innerHTML === '' ? 'active' : ''}">
                 <div class="row justify-content-center">
-                    <div class="col-md-4">
+                    <div class="col-md-5 p-5">
                         <div class="card d-flex flex-column mb-3 h-100">
-                            <img src="${projet.image}" class="card-img-top" alt="${projet.title}">
+                            <img src="${projet.image}" class="card-img-top img-fluid" style="height:200px; object-fit:cover;" alt="${projet.title}">
                             <div class="card-body d-flex flex-column">
                                 <h5 class="card-title mb-2">${projet.title}</h5>
                                 <p class="card-text mb-3">
@@ -61,7 +68,7 @@ function createProjectCard(screenSize) {
             projectContainer.innerHTML += `
             <div class="carousel-item ${i === 0 ? 'active' : ''}">
                 <div class="row justify-content-center">
-                    <div class="col-md-4">
+                    <div class="col-md-5">
                         <div class="card d-flex flex-column mb-3 h-100">
                             <img src="${projet1.image}" class="card-img-top" alt="${projet1.title}">
                             <div class="card-body d-flex flex-column">
@@ -74,7 +81,7 @@ function createProjectCard(screenSize) {
                             </div>
                         </div>
                     </div>
-                    <div class="col-md-4">
+                    <div class="col-md-5">
                         <div class="card d-flex flex-column mb-3 h-100">
                                 <img src="${projet2.image}" class="card-img-top" alt="${projet2.title}">
                                 <div class="card-body d-flex flex-column">
